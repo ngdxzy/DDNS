9cc84aec42f1b179b75a59b1cf1b7a8a533585b0
ngdxzy <zhenyu_xu@uri.edu>
2022-04-20 22:05:11 -0400

    someUtil

---
+++ server/bind9_utils.py
@@ -0,0 +1,66 @@
+## APIs to create required bind9 files
+from ast import expr_context
+import shutil as sh
+import os
+
+def create_zone(name):
+    if os.path.exists('./zones.' + name):
+        return
+        
+    with open("./zones.template","r") as template_file, open("./zones." + name, "w") as new_zone_file:
+        for line in template_file:
+            new_zone_file.write(line.replace('TOREPLACE',name))
+
+    sh.copy("db.template", "db." + name, )
+
+    with open("named.conf.local","r") as cfg_old, open("named.conf.local.new","w") as cfg_new:
+        flag = False
+        for line in cfg_old:
+            cfg_new.write(line)
+            if '.' + name + '\"' in line:
+                flag = True
+        if not flag:
+            cfg_new.write('\ninclude \"/etc/bind/zones.%s\";' % name)
+    
+    sh.move("named.conf.local.new", "named.conf.local")
+    
+def bind(zone, domain_name, IP):
+    # add forward record
+    with open('db.' + zone,"r") as db_file_old, open('db.' + zone + ".temp","w") as db_new_file:
+        for line in db_file_old:
+            if domain_name + '.' + zone + '.' not in line:
+                db_new_file.write(line)
+            if "NS records" in line:
+                db_new_file.write("\tIN NS %s.\n" % (domain_name + '.' + zone))
+            if "NS mapping" in line:
+                db_new_file.write("%s.\tIN A %s\n" % ((domain_name + '.' + zone), IP))
+
+    sh.move('db.' + zone + ".temp", 'db.' + zone)
+
+
+def unbind(zone, domain_name):
+    with open('db.' + zone,"r") as db_file_old, open('db.' + zone + ".temp","w") as db_new_file:
+        for line in db_file_old:
+            if domain_name not in line:
+                db_new_file.write(line)
+     
+    sh.move('db.' + zone + ".temp", 'db.' + zone)
+
+def remove_zone(name):
+    try:
+        os.remove('db.' + name)
+    except:
+        pass
+    try:
+        os.remove('zones.' + name)
+    except:
+        pass
+    # os.remove('/etc/bind/db.' + name)
+    # os.remove('/etc/bind/zones.' + name)
+    with open("named.conf.local","r") as cfg_old, open("named.conf.local.new","w") as cfg_new:
+        for line in cfg_old:
+            if '.' + name + '\"' not in line:
+                cfg_new.write(line)
+
+    sh.move("named.conf.local.new", "named.conf.local")
+    # sh.move("named.conf.local", "/etc/bind/named.conf.local")
---
+++ server/user_manager.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from datetime import datetime
+def get_time():
+    return datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+def register_user(user_name, zone, subnet):
+    table = pd.read_csv('registered_users.csv')
+    print(table)
+    if user_name not in table['username'].to_list():
+        new_record = {}
+        new_record['username'] = [user_name]
+        new_record['zone'] = [zone]
+        new_record['subnet'] = [subnet]
+        new_record['ip'] = ['x']
+        new_record['date'] = [get_time()]
+        new_record = pd.DataFrame(new_record)
+        table = pd.concat([table, new_record],ignore_index=True)
+
+        print(table)
+        table.to_csv('registered_users.csv',index=False)
+
+def remove_user(user_name):
+    table = pd.read_csv('registered_users.csv')
+    if user_name in table['username'].to_list():
+        table.drop(table['username'].to_list().index(user_name), inplace=True)
+        table.to_csv('registered_users.csv',index=False)
+
+def update_info(user_name, new_ip):
+    table = pd.read_csv('registered_users.csv')
+    if user_name in table['username'].to_list():
+        idx = table['username'].to_list().index(user_name)
+        table['ip'][idx] = new_ip
+        table['date'][idx] = get_time()
+        table.to_csv('registered_users.csv',index=False)
