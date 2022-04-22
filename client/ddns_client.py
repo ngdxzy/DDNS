61f1280d567db875e896749158a97379bfd42fee
ngdxzy <zhenyu_xu@uri.edu>
2022-04-21 21:27:30 -0400

    rmove_prt

--- client/ddns_client.py
+++ client/ddns_client.py
@@ -1,39 +1,61 @@
-from multiprocessing.pool import RUN
-import net_utils
 import threading as th
-from queue import Queue
 import socket as ss
 import time
-def tcp_server(port, msg_q):
-    pass
+import re
+from netifaces import interfaces, ifaddresses, AF_INET
 
-def udp_clinet(name, eth_name, port, IP, msg_q):
-    HOST = IP
+def get_valid_ip(eth_name):
+    addresses = {}
+    for ifaceName in interfaces():
+        addresses[ifaceName] = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'-1.-1.-1.-1'}] )]
+    #print(addresses)
+    for itf_name, ip in addresses.items():
+        if itf_name == eth_name:
+            return ip[0]
+    return '255.255.255.255'
+
+def tcp_server(port, name):
+    HOST = ''
     PORT = port
+    ADDR = (HOST, PORT)
+    tcpServer = ss.socket(ss.AF_INET, ss.SOCK_STREAM)
+    try:
+        tcpServer.bind(ADDR)
+    except:
+        # in case the address is taken by old threads, if so , reuse the address
+        tcpServer.setsockopt(ss.SOL_SOCKET, ss.SO_REUSEADDR, 1)
+        # connect again
+        tcpServer.bind(ADDR)
+    tcpServer.listen(1)
+    while True:
+        tcpConnection, addr = tcpServer.accept()
+        data = tcpConnection.recv(1024)
+        tcpConnection.send(name.encode('utf-8'))
+        tcpConnection.close()
+
+def udp_clinet(name, eth_name, udpClient):
+    valid_ip = get_valid_ip(eth_name)
+    msg = name + ' ' + valid_ip
+    try:
+        udpClient.sendto(msg.encode('utf-8'), ADDR)
+    except:
+        pass
+
+if __name__ == "__main__":
+    cfg = open('client.cfg')
+    local_name = cfg.readline().strip()
+    eth_name = cfg.readline().strip()
+    server_ip = cfg.readline().strip()
+    cfg.close()
+
+    ddns_client_service = th.Thread(target=tcp_server, args=(60001, local_name,))
+    ddns_client_service.start()
+    HOST = server_ip
+    PORT = 60000
     ADDR = (HOST, PORT)
     udpClient = ss.socket(ss.AF_INET, ss.SOCK_DGRAM, 0)
     udpClient.settimeout(1)
-    RUN_FLAG = True
-    while RUN_FLAG:
-        try:
-            valid_ip = net_utils.get_valid_ip(eth_name)
-            msg = name + ' ' + valid_ip
-            udpClient.sendto(msg.encode('utf-8'), ADDR)
-        except:
-            pass
-        for i in range(60):
-            if not msg_q.empty():
-                cmd = msg_q.get()
-                if cmd == "exit":
-                    RUN_FLAG = False
-            time.sleep(1)
-
-if __name__ == "__main__":
-    msg_q_tcp = Queue()
-    msg_q_udp = Queue()
-    cfg = open('client.cfg')
-    local_name = cfg.read()
-    server_ip = cfg.read()
-    cfg.close()
-
+    while True:
+        udp_clinet(local_name, eth_name, udpClient)
+        time.sleep(10)
 
